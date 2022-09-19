8a8cf4b7c7e600e45a39de70750c1696723b4e76
LeandroRomeroFuentes <leandroromerofuentes@gmail.com>
2022-09-19 12:42:18 -0300

    cambio logo

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -4,7 +4,7 @@ import { Link } from "react-router-dom";
 const Header = () => {
     return (
         <header className="header">
-            <img src="/logo512.png" alt="logo" className="logo" />
+            <img src="/logoMaedre.png" alt="logo" className="logo" />
             <Link to="/"><h1 className="titulo">Maedre</h1></Link>
             <NavBar type="header" />
         </header>
